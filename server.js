f66ba382906f1eb5d3d7ef7b7b8d619c7746c03c
Pranay Lobo <42401887+pranaylobo@users.noreply.github.com>
2021-04-29 17:35:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var nodemailer = require('nodemailer');
 
-const PORT =  3000;
+const PORT =  process.env.PORT || 3000;
 const cors = require('cors')
 var firebase = require('firebase');
 
